deb59204f8323211f53350d033579f8b3dd8a546
tshifhiwasinugo <84003447+tshifhiwasinugo@users.noreply.github.com>
2021-06-20 17:24:54 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+// Define a user and use all the details and define an address as an object with proprties such as street number, suburb, city, Country using OOP.
+// Display each detail on the console or table format.
+
+class User {
+   constructor(name, surname, age, gender,){
+     this.name = name;
+     this.surname = surname;
+     this.age = age;
+     this.gender = gender;
+
+
+   }
+   
+      getUserDetails(na, sur, ag, gen){
+        
+       return na + " " + sur + " his age is "+ ag+" " + "and"+" "+"his gender is "  + gen;
+   }
+  
+} 
+
+   var U = new User();
+
+   console.table(U.getUserDetails("Lunah", "sinugo", 24, "male"));
+
+class Address extends User {
+   constructor(streetnum, surbub, city, countryy){
+     super();
+     this.steetnum = streetnum ;
+     this.surbub = surbub;
+     this.city = city;
+     this.coutryy = countryy;
+
+   }
+   
+      getAddressDetails(streetnum, surbub, city, countryy){
+        
+       return "street number is"+ " "+ streetnum + " " + surbub + " "+ city +" "+ countryy;
+   }
+  
+} 
+
+ const Adde = new Address();
+ console.log(Adde.getAddressDetails( 13, "bendor", "polokwane" , "South Africa" ));
